2444020dab154b216698fccdfb085eb4e377fe70
Veeresh Ediga <120732345+VeereshE@users.noreply.github.com>
2023-04-19 13:15:12 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import BoxContainer from "./Components/BoxContainer"
+import ButtonContainer from "./Components/ButtonContainer"
+import './App.css';
+
+function App() {
+  return (
+    <>
+        <BoxContainer/>
+        <ButtonContainer/>
+    </>
+  );
+}
+
+export default App;
